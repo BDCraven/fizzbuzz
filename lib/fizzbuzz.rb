aec195ce9216ab398c292fe7e25a99b3b8fc8ccc
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-28 12:01:11 +0100

    Accepts number from command line and performs the same actions

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,10 @@ def fizzbuzz(number)
   return "Fizzbuzz" if number % 15 == 0
   return "Fizz" if number % 3 == 0
   return "Buzz" if number % 5 == 0
-  number 
+  number
+end
+
+if ARGV.first != nil
+  number = ARGV.first.to_i
+  puts "#{number} --> #{fizzbuzz(number)}"
 end
