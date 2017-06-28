36d5c2fdf76e0f7c560d18303bfbf205748d3cff
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-28 11:29:34 +0100

    Fails to return number when not a multiple of 3, 5 or 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,8 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'Fizz'
+    n = [3,6,9,12,18,21,24,27].sample
+    expect(fizzbuzz(n)).to eq 'Fizz'
   end
   it 'returns "Buzz" when passed 5' do
     n = [5,10,20,25,35].sample
@@ -11,5 +12,9 @@ describe 'fizzbuzz' do
   it 'returns "Fizzbuzz" when passed 15' do
     n = [15,30,45,60,75].sample
     expect(fizzbuzz(n)).to eq 'Fizzbuzz'
+  end
+  it 'returns the number otherwise' do
+    n = [1,2,4,7,8,11,13,14,16,17,19].sample
+    expect(fizzbuzz(n)).to eq n
   end
 end
