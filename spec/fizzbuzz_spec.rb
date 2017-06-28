18d9965b4ffb956159a0e8527bf3e3b770def687
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-06-28 11:12:34 +0100

    Failed to Buzz when passed a multiple of 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'Fizz'
   end
+  it 'returns "Buzz" when passed 5' do
+    n = [5,10,20,25,35].sample
+    expect(fizzbuzz(n)).to eq 'Buzz'
+  end
 end
